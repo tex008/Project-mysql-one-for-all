03e4b88a11c900f1c87481eb92103ef4e278d332
Vinicius Teixeira Freitas <texera009@gmail.com>
2022-07-25 11:41:59 -0300

    feat: Adiciona requisito 9 ao projeto. Exibe a quantidade de musicas reproduzidas no historico do usuario Bill

---
+++ desafio9.sql
@@ -0,0 +1,7 @@
+SELECT 
+    COUNT(h.usuario_id) AS quantidade_musicas_no_historico
+FROM
+    SpotifyClone.historico_reproducoes AS h
+    INNER JOIN SpotifyClone.usuarios AS u
+    ON h.usuario_id = u.usuario_id
+    WHERE u.usuario = 'Bill';
